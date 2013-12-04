a845526470eb0954dd83972a1dde418f3b8becd7
Timo Lindemann <timo.lindemann@sol-venetus.de>
2013-12-04 22:35:15 +0100

    Added the browser test suite and instructions on how to extend it.

---
+++ browser/README.md
@@ -0,0 +1,13 @@
+# Browser test suite
+
+*mocha.css* and *mocha.js* are needed here; you can get them from the mocha
+project:
+
+    npm install mocha
+
+just put them into this directory. Any webserver will do; I've used gatling.
+
+## Extending the browser test suite:
+
+1. add your tests to alltests.js.
+2. run `grunt browsersuite`. It will bundle up the tests. 
---
+++ browser/alltests.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lindem on 12/4/13.
+ */
+
+require("../tests/TestArrayContracts");
+require("../tests/TestBasicContracts");
+require("../tests/Testcontractify");
+require("../tests/TestDateContracts");
+require("../tests/TestNonNegativeInteger");
+require("../tests/TestObjectContracts");
+require("../tests/TestTypeContracts");
+require("../tests/TestStringContracts");
